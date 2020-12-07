1a19f0919cbef3bcda555795716b7fc0a1a40e83
Samundra Acharya <54012620+Samundra-032@users.noreply.github.com>
2020-12-07 16:31:39 +0545

    Add files via upload

---
+++ src/Light.cpp
@@ -0,0 +1,23 @@
+#include "Light.h"
+
+Light::Light()
+{
+	colour = glm::vec3(1.0f, 1.0f, 1.0f);
+	ambientIntensity = 1.0f;
+}
+
+Light::Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity)
+{
+	colour = glm::vec3(red, green, blue);
+	ambientIntensity = aIntensity;
+}
+
+void Light::UseLight(GLfloat ambientIntensityLocation, GLfloat ambientColourLocation)
+{
+	glUniform3f(ambientColourLocation, colour.x, colour.y, colour.z);
+	glUniform1f(ambientIntensityLocation, ambientIntensity);
+}
+
+Light::~Light() 
+{
+}
