895646c1c8e54190d42388f04360623e79298abf
Samundra Acharya <54012620+Samundra-032@users.noreply.github.com>
2020-12-08 16:28:28 +0545

    Update Light.h

--- include/Light.h
+++ include/Light.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include"defines.h"
-
 class Light
 {
 public:
 	Light();
-	Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity);
+	Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity,
+		GLfloat xDir, GLfloat yDir, GLfloat zDir, GLfloat dIntensity);
 
-	void UseLight(GLfloat ambientIntensityLocation, GLfloat ambientColourLocation);
-
+	void UseLight(GLfloat ambientIntensityLocatioin, GLfloat ambientColourLocation,
+		GLfloat diffuseInteensitylocation, GLfloat directionlocation);
 	~Light();
 
 private:
 	glm::vec3 colour;
 	GLfloat ambientIntensity;
-};
 
+	glm::vec3 direction;
+	GLfloat diffuseIntennsity;
+};
